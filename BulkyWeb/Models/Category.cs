d631f822facc2e9403d92c33d92bc55d932eda9f
DinanDias <93309200+DinanDias@users.noreply.github.com>
2024-10-20 10:17:08 +0530

    added server side validations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -8,9 +8,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)] //server-side
         [DisplayName("Category Name")] //client side validation adjusting row names
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order should be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
